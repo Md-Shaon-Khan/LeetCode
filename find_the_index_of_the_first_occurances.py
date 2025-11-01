d35b647c1ea46345e747300786b3144492e55bb0
Md-Shaon-Khan <shaon.iit52@gmail.com>
2025-11-01 10:27:55 +0600

    First index of a given string

---
+++ find_the_index_of_the_first_occurances.py
@@ -0,0 +1,17 @@
+class Solution(object):
+    def strStr(self, haystack, needle):
+        """
+        :type haystack: str
+        :type needle: str
+        :rtype: int
+        """
+        n, m = len(haystack), len(needle)
+        
+        if m == 0:
+            return 0  
+        
+        for i in range(n - m + 1):
+            if haystack[i:i+m] == needle:
+                return i
+        
+        return -1
