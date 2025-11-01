5a96d0da7c87aa9746f66c4eddf26dc04d911284
Md-Shaon-Khan <shaon.iit52@gmail.com>
2025-11-01 22:17:45 +0600

    insertion sort

---
+++ insertion_sort_list.py
@@ -0,0 +1,33 @@
+class ListNode(object):
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+class Solution(object):
+    def insertionSortList(self, head):
+        
+        if not head:
+            return None
+        
+        arr = []
+        current = head
+        while current:
+            arr.append(current.val)
+            current = current.next
+        
+        for i in range(1,len(arr)):
+            key = arr[i]
+            j = i - 1
+            
+            while j >= 0 and key<arr[j]:
+                arr[j+1] = arr[j]
+                j -= 1
+            arr[j+1] = key
+        
+        dummy = ListNode(0)
+        current = dummy
+        for value in arr:
+            current.next = ListNode(value)
+            current = current.next
+        
+        return dummy.next
